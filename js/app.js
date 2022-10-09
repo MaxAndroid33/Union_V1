bcc613e6a4d578be8e08b04082a9bec67f8a3d79
MaxAndroid33 <yuosifmax309@gmail.com>
2022-10-10 01:30:13 +0300

    complate the menu

--- js/app.js
+++ js/app.js
@@ -1,28 +1,23 @@
 
 const menu =document.getElementById('menu');
-const navbar =document.getElementById('navbar');
-// const menu =document.querySelector('.menu');
+const navbar =document.getElementById('nav-under');
+const menuitems =document.querySelector('.menu-items');
 
 
 // PX offset when the navbar active
 const offset =200;
 
 
-
-
-
-
-
-
 window.addEventListener('scroll',function(){
 
     if (this.pageYOffset > offset ){
         navbar.classList.add('navbar-active');
+        menuitems.classList.add('menu-items-active');
       
         
     }
     else{
         navbar.classList.remove('navbar-active');
-       
+       menuitems.classList.remove('menu-items-active');
     }
 });
